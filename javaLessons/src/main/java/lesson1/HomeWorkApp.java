0c2c9c316b0ea4be13a288fb0db1644741d3afdc
matmerian <matmerian@yandex.ru>
2022-08-14 23:12:42 +0300

    add lesson1

---
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,46 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10, b = -4;
+        if ((a + b) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 14;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 4, b = 11;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
---
+++ javaLessons/src/main/java/lesson1/TrainClass.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+public class TrainClass {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            drawCat();
+        }
+    }
+
+    public static void drawCat() {
+        System.out.println("       	_..---...,\"\"-._     ,/}/)\n" +
+                "    	.''    	,      ``..'(/-<\n" +
+                "   	/   _  	{  	)     	\\\n" +
+                "  	;   _ `. 	`.   <     	a(\n" +
+                "	,'   ( \\  )      `.  \\ __.._ .: y\n" +
+                "   (  <\\_-) )'-.____...\\  `._   //-'\n" +
+                "	`. `-' /-._)))  	`-._)))\n" +
+                "  	`...'     	");
+    }
+
+}
